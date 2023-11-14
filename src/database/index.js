b23916572c3b8fd9972a0be6f5993a791a14e457
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-14 20:50:57 -0300

    Alterando link de conexão do mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://127.0.0.1:27017/car-rental', {
+mongoose.connect('mongodb://localhost:27017/car-rental', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
