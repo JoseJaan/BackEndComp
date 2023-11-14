a8b90f9f1484d87a492a9cf110dd903d4e2b4f05
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-14 14:37:20 -0300

    Alterando export no index do controllers

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,7 +1,13 @@
-import Garage from "./Garage";
+/*import Garage from "./Garage";
 import Auth from "./Auth";
 import Rentals from "./Rentals"
 import Records from "./Records"
 import Uploads from "./Uploads"
 
-export{Garage, Auth, Rentals, Records, Uploads};
+export{Garage, Auth, Rentals, Records, Uploads};*/
+
+export { default as Garage } from './Garage';
+export { default as Auth } from './Auth';
+export { default as Rentals } from './Rentals';
+export { default as Records } from './Records';
+export { default as Uploads } from './Uploads';
