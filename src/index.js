a44756f36eab1c918299c2a8c658c000d976b4c5
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-14 14:47:44 -0300

    Desfazendo alterações no index dos controllers e no principal. Alterando start do package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express, {Router} from "express";
 import bodyParser from "body-parser";
-//import { Garage, Auth, Rentals, Records, Uploads } from "./app/controllers";
-import * as controllers from "./app/controllers";
+import { Garage, Auth, Rentals, Records, Uploads } from "./app/controllers";
 
 const app = express();
 const port = 8080;
@@ -9,11 +8,11 @@ const port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/garage', controllers.Garage);
-app.use('/auth',controllers.Auth);
-app.use('/rentals',controllers.Rentals);
-app.use('/records', controllers.Records);
-app.use('/uploads', controllers.Uploads)
+app.use('/garage', Garage);
+app.use('/auth',Auth);
+app.use('/rentals',Rentals);
+app.use('/records',Records);
+app.use('/uploads', Uploads)
 
 console.log(`Servidor rodando no link http://localhost:${port}`);
 
