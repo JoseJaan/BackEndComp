224d6c6b3311b767f1f487a966267776aaf7d7bd
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-14 14:33:43 -0300

    Alterando o import e o app.use no index principal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express, {Router} from "express";
 import bodyParser from "body-parser";
-import { Garage, Auth, Rentals, Records, Uploads } from "./app/controllers";
+//import { Garage, Auth, Rentals, Records, Uploads } from "./app/controllers";
+import * as controllers from "./app/controllers";
 
 const app = express();
 const port = 8080;
@@ -8,11 +9,11 @@ const port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/garage', Garage);
-app.use('/auth',Auth);
-app.use('/rentals', Rentals);
-app.use('/records', Records);
-app.use('/uploads', Uploads)
+app.use('/garage', controllers.Garage);
+app.use('/auth',controllers.Auth);
+app.use('/rentals',controllers.Rentals);
+app.use('/records', controllers.Records);
+app.use('/uploads', controllers.Uploads)
 
 console.log(`Servidor rodando no link http://localhost:${port}`);
 
