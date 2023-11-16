382e8b71b9159542fbe17e1aedd0d3f1eadc0a0e
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-16 20:24:21 -0300

    Arrumando erros da rota update-car

--- src/app/controllers/Garage.js
+++ src/app/controllers/Garage.js
@@ -185,7 +185,7 @@ router.put(
   if(VerifyLicensePlate(licensePlate)){
     Cars.findOne({licensePlate})
     .then(exist =>{
-      if(exist){
+      if(!exist){
         Cars.findById(req.params.carId)
           .then((car) => {
             if (!car) {
