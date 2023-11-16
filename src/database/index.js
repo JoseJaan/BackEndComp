456dc3b451d1c4f7a3cb30d639e96758175164d5
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-16 19:04:56 -0300

    Alterando import do .env no index da database

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
-require('dotenv.cjs').config();
+//require('dotenv.cjs').config();
+import dotenv from 'dotenv'
 
-
+dotenv.config();
 //mongoose.connect('mongodb://127.0.0.1:27017/car-rental',
 
 //const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/car-rental';
