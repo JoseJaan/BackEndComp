75ce91652df8cb8c1999909332c943cb508e4fdb
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-27 19:26:39 -0300

    Correções no Clodinary

--- src/app/controllers/Garage.js
+++ src/app/controllers/Garage.js
@@ -8,6 +8,7 @@ import isAdmin from '../middlewares/isAdmin.js';
 import path from 'path';
 import cloudinary from '../../database/cloudinary.config.js';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
 
 const router = new Router();
 
@@ -381,7 +382,7 @@ router.delete('/delete-car/:carId', [isAuthenticated, isAdmin], (req, res) => {
     }
   },
 );*/
-const multerCloudinary = Multer({ storage });
+const multerCloudinary = multer({ storage });
 
 // Sua rota utilizando Multer com Cloudinary
 router.post(
