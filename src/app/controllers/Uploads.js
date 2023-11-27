b39c630efd6bf06ba8610d7389bc96d990aaf9a1
José Neto <joseacerbialmeidaneto@hotmail.com>
2023-11-27 20:01:03 -0300

    Corrigindo cloudinary

--- src/app/controllers/Uploads.js
+++ src/app/controllers/Uploads.js
@@ -9,15 +9,20 @@ const router = new Router();
 //tem que ser so adm
 //Na verdade acho que não precisa ser só admin -- 04/11 - 08:43
 router.get('/images/:filename', [isAuthenticated, isAdmin], (req, res) => {
-  const filePath = path.resolve(`./uploads/images/${req.params.filename}`);
+  const cloudinaryBaseUrl =
+    'https://res.cloudinary.com/seu-cloud-name/image/upload/';
+  const imageUrl = cloudinaryBaseUrl + req.params.filename;
 
-  fs.access(filePath, fs.constants.F_OK, (err) => {
+  res.redirect(imageUrl);
+  //const filePath = path.resolve(`./uploads/images/${req.params.filename}`);
+
+  /*fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       return res.status(404).send({ error: 'Arquivo não encontrado' });
     } else {
       return res.sendFile(filePath);
     }
-  });
+  });*/
 });
 
 export default router;
